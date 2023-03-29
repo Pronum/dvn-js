37373ac8a0155792bae042c7020266fd262d69db
st3d <99141995+ist3d@users.noreply.github.com>
2023-03-29 05:06:57 +0200

    More editing needed for multi dependencies, But good for now

--- src/util/commands/init/mod.rs
+++ src/util/commands/init/mod.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::{fs, io::{stdin, stdout, Write}, path::Path};
 use colored::Colorize;
 use std::env;
 
@@ -11,8 +11,12 @@ pub fn run(_argument: String) {
     let nmut_pkg_name         = String::from(nmut_dir_path.to_string()).split("\\").last().unwrap().to_string();
 
     fn skip_terminal_io(dmut_dir_path: String, nmut_file_name: &str, nmut_pkg_name: String, nmut_dir_path: std::path::Display) {
+        if Path::new(&String::from(dmut_dir_path.clone() + nmut_file_name)).exists() {
+            return println!("{} {}", "Project is already initialized!".bright_black().bold(), "If you still want to create new package.json remove the old one and rerun the command!".red().bold());
+        }
+
         fn default_file_content(package_name: &str) -> String {
-            return format!("{}\n   \"name\": \"{}\",\n   \"version\": \"1.0.0\",\n   \"author\": \"Unspecified\"\n{}", "{", package_name, "}").to_string();
+            return format!("{}\n  \"name\": \"{}\",\n  \"version\": \"1.0.0\",\n  \"author\": \"Unspecified\"\n{}", "{", package_name, "}").to_string();
         }
     
         fs::write(dmut_dir_path + nmut_file_name, default_file_content(nmut_pkg_name.as_str()).to_string()).ok();
@@ -25,10 +29,47 @@ pub fn run(_argument: String) {
         "--yes"    => skip_terminal_io(dmut_dir_path, nmut_file_name, nmut_pkg_name, nmut_dir_path),
         "--ignore" => skip_terminal_io(dmut_dir_path, nmut_file_name, nmut_pkg_name, nmut_dir_path),
         _ => {
-            println!("{}", "See you later baby!".bold().red());
-            // Command line questions like Name: blablabla
-            // Version: 1.0.0
-            // Author: Unspecified
+            if Path::new(&String::from(dmut_dir_path.clone() + nmut_file_name)).exists() {
+                return println!("{} {}", "Project is already initialized!".bright_black().bold(), "If you still want to create new package.json remove the old one and rerun the command!".red().bold());
+            }
+
+            print!("{} ({}) {} ", "Enter Package Name".bold().black(), nmut_pkg_name.bold().red(), ":".bold().bright_black());
+            let _first_flush   = stdout().flush();
+            let mut package_name_input    = String::new();
+            stdin().read_line(&mut package_name_input).expect("Using default directory name.");
+
+            print!("{} ({}) {} ", "Enter Package Version".bold().black(), "1.0.0".bold().red(), ":".bold().bright_black());
+            let _middle_flush   = stdout().flush();
+            let mut package_version_input = String::new();
+            stdin().read_line(&mut package_version_input).expect("Using default version.");
+
+            print!("{} ({}) {} ", "Enter Package Author".bold().black(), "Unspecified".bold().red(), ":".bold().bright_black());
+            let _last_flush   = stdout().flush();
+            let mut package_author_input = String::new();
+            stdin().read_line(&mut package_author_input).expect("Using default author.");
+
+            if package_name_input.is_empty() || package_name_input.trim().len() < 1 {
+                package_name_input = nmut_pkg_name;
+            }
+
+            if package_version_input.is_empty() || package_version_input.trim().len() < 1 {
+                package_version_input = "1.0.0".to_string();
+            }
+
+            if package_author_input.is_empty() || package_author_input.trim().len() < 1 {
+                package_author_input = "Unspecified".to_string();
+            }
+
+            fn default_file_content(package_name_input: String, package_version_input: String, package_author_input: String) -> String {
+                return format!("{}\n  \"name\": \"{}\",\n  \"version\": \"{}\",\n  \"author\": \"{}\"\n{}", "{",  package_name_input.trim().to_lowercase().to_string().chars()
+                .map(|x| match x { 
+                    ' ' => '-',
+                    _ => x
+                }).collect::<String>(), package_version_input.trim(), package_author_input.trim(), "}").to_string();
+            }
+        
+            fs::write(dmut_dir_path + nmut_file_name, default_file_content(package_name_input, package_version_input, package_author_input).to_string()).ok();
+            println!("{} \"{}\" {}", "Done initializing your project".bright_black().bold(), nmut_dir_path.to_string().bright_yellow().bold(), ".");
         }
     }
 }
--- src/util/commands/install/mod.rs
+++ src/util/commands/install/mod.rs
@@ -1,9 +1,9 @@
-use std::{fs, collections::HashMap};
+use std::{fs, path::Path, collections::HashMap};
 use colored::Colorize;
 
 use serde::{Deserialize, Serialize};
 use reqwest::blocking::get;
-use serde_json::Value;
+use serde_json::{Value};
 
 use std::env;
 
@@ -11,8 +11,11 @@ pub fn run(package_name: String, package_paramater: String) {
     let env_path = env::current_dir().unwrap().to_owned();
     
     let dmut_dir_path: String          = env_path.clone().display().to_string();
-    let nmut_dir_path         = env_path.display();
-    let nmut_pkg_main: &str           = "/node_modules/";
+    let _nmut_pkg_main: &str           = "/node_modules/";
+
+    if !Path::new(&String::from(dmut_dir_path.clone() + "/package.json".to_string().as_str())).exists() {
+        return println!("{} {}", "Project is not initialized!".bright_black().bold(), "Please use \"init\" command before installing a package.".bright_cyan().bold());
+    }
 
     let uri = format!("{}/{}", "https://registry.npmjs.org/", package_name).to_string();
     let res = get(String::from(uri)).unwrap();
@@ -42,8 +45,67 @@ pub fn run(package_name: String, package_paramater: String) {
         println!("{}: {} {}{}{}", "registry.npmjs.org".bold().bright_cyan(), "Configuring".bold().bright_black(), package_name.bold().bright_magenta(), "@".bold(), registry_data_latest_ver.clone().to_string().bold().bright_magenta());
         println!("{}: {} {} {}", "registry.npmjs.org".bold().bright_cyan(), "Tarball".bold().bright_black(), "#".bold(), registry_data_latest_tar.bold().bright_magenta());
         
-        // Please continue working on functionality!
         // Create node_modules if not found
+
+        let node_modules_strp = String::from(dmut_dir_path.clone() + _nmut_pkg_main.clone());
+        let node_modules_path  = Path::new(node_modules_strp.as_str());
+        let node_modules_clone = node_modules_path.clone();
+
+        if !node_modules_clone.exists() {
+            fs::create_dir(node_modules_clone).unwrap_or_default();
+            println!("{}: {} {} {}", "dvn.base.dir".bold().bright_cyan(), "Created".bold().bright_black(), "#".bold(), "/node_modules/".bold().bright_yellow());
+        }
+
+        println!("{}: {} {}{}{} {}", "registry.npmjs.org".bold().bright_cyan(), "Adding module".bold().bright_black(), package_name.bold().bright_magenta(), "@".bold(), registry_data_latest_ver.clone().to_string().bold().bright_magenta(), "to package.json".bold().bright_black());
+
+        let parsed_file_path    = String::from(dmut_dir_path.clone() + "/package.json".to_string().as_str());
+        let parsed_file_content = fs::read_to_string(parsed_file_path.clone());
+
+        pub fn merge(v: &Value, fields: &HashMap<String, Value>) -> Value {
+            match v {
+                Value::Object(m) => {
+                    let mut m = m.clone();
+                    for (k, v) in fields {
+                        m.insert(k.clone(), Value::Object(v.as_object().unwrap().clone()));
+                    }
+                    Value::Object(m)
+                }
+                v => v.clone(),
+            }
+        }
+
+        #[allow(unused_variables)]
+        pub fn module_save(package_name: String, package_version: String, parsed_file_path: String, parsed_file_content: String) {
+            #[derive(Debug, Serialize, Deserialize)]
+            struct PackageJsonFileStruct {
+                dependencies: Value
+            }
+            
+            let package_json: serde_json::Value = serde_json::from_str(&parsed_file_content).expect("package.json file was not formatted correctly!");
+            let mut dependencie = HashMap::new();
+            let mut package = serde_json::Map::new();
+            package.insert(package_name.clone(), Value::String("^".to_string() + package_version.as_str()));
+            dependencie.insert("dependencies".to_string(), Value::Object(package));
+
+            let merged_data = merge(&package_json, &dependencie);
+
+            fs::write(parsed_file_path, serde_json::to_string_pretty(&merged_data).unwrap().to_string()).unwrap();
+
+            println!("{}: {} {}{}{} {}", "registry.npmjs.org".bold().bright_cyan(), "Added module".bold().bright_black(), package_name.clone().bold().bright_magenta(), "@".bold(), package_version.clone().to_string().bold().bright_magenta(), "to package.json".bold().bright_black());
+        }
+
+        #[allow(unused_variables)]
+        pub fn module_save_dev(package_name: String, package_version: String, parsed_file_path: String, parsed_file_content: String) {
+            let package_json: serde_json::Value = serde_json::from_str(&parsed_file_content).expect("package.json file was not formatted correctly!");
+
+        }
+
+        match package_paramater.as_str() {
+            "--save"     => module_save    (package_name, registry_data_latest_ver, parsed_file_path, parsed_file_content.unwrap()),
+            "--save-dev" => module_save_dev(package_name, registry_data_latest_ver, parsed_file_path, parsed_file_content.unwrap()),
+            _            => module_save    (package_name, registry_data_latest_ver, parsed_file_path, parsed_file_content.unwrap()),
+        }
+
         // Download data from registry_data_latest_tar
         // Create a folder inside the node_modules on the name of the package 
         // Donwload the package in node_modules/folder_of_the_package and extract it 
@@ -54,6 +116,8 @@ pub fn run(package_name: String, package_paramater: String) {
         // Compress node_modules/folder_of_the_package
         // When using "dvn run script --unpack-cached" or "dvn script -u-c" it 
         // will unpack the compressed folder then it will run node process in the background
+        // Then add it as a (dev or not) dependencie(s) in package.json
+        // If no package.json then just alert the user 🤷‍♂️
 
     }
 }
